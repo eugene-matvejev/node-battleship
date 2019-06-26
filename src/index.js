c4869f39eb8d3cbea8747c37627651075e4f8d1e
Eugene Matvejev <eugene.matvejev@gmail.com>
2019-06-26 16:03:02 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 const { ApolloServer, gql } = require('apollo-server');
 const models = require('./models');
-const crypto = require('crypto');
-// const Sequelize = require('sequelize');
 
-// Option 1: Passing parameters separately
-// const sequelize = new Sequelize('database', 'username', 'password', {
-//   host: 'localhost',
-//   dialect: /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
-// });
-
-// Option 2: Passing a connection URI
-// const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/game');
 models.sequelize
     // .sync({ force: true })
     .sync()
-    // .authenticate()
     .then(() => {
-        // const args = arguments;
-        // console.log(args);
-        // const a = crypto.createHmac('sha256', 'secret').update('password').digest('hex');
-
-        // debugger;
         console.log('Connection has been established successfully.');
     })
     .catch(err => {
@@ -30,8 +14,6 @@ models.sequelize
 // The GraphQL schema
 const typeDefs = gql`
     type Query {
-        "A simple type for getting started!"
-        hello: String
         allUsers: [User]
     }
 
@@ -44,11 +26,6 @@ const typeDefs = gql`
 // A map of functions which return data for the schema.
 const resolvers = {
     Query: {
-        hello: () => {
-            const ar = arguments;
-            debugger;
-            return 'world';
-        },
         allUsers: (_0, _1, { models }, _3) => {
             debugger;
 
