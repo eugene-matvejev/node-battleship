decab6e1cbbe30ddc5cbbbf4241252a9119b238e
Eugene Matvejev <eugene.matvejev@gmail.com>
2019-07-04 15:07:40 +0100

    dotenv + nodemon

--- config/config.js
+++ config/config.js
@@ -1,17 +1,10 @@
-const config = {
-    host: process.env.DB_HOSTNAME || '127.0.0.1',
-    username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || 'password',
-    database: process.env.DB_NAME || 'game',
-    dialect: 'mysql',
-}
-
 module.exports = {
-    production: config,
-    development: config,
-    test: {
-        dialect: 'sqlite',
-        storage: '../var/database.sqlite'
+    database: {
+        host: process.env.DB_HOSTNAME,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        dialect: process.env.DB_DIALECT,
     },
     secret: process.env.SECRET_KEY,
-};
+}
