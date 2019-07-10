4b14b1a7f1f207b3aab384b5078ed7411f96ef45
Eugene Matvejev <eugene.matvejev@gmail.com>
2019-07-10 14:36:36 +0100

    fix test

--- src/graphql/cell.test.js
+++ src/graphql/cell.test.js
@@ -5,95 +5,96 @@ import orm from '../orm';
 import { typeDefs, resolvers } from '../graphql';
 
 describe('GraphQL Type: Cell', () => {
-    // const server = new ApolloServer({
-    //     typeDefs,
-    //     resolvers,
-    //     dataSources: () => ({}),
-    //     context: () => ({ orm }),
-    // });
+    // (new Uint8Array(2)).forEach((_, i) => {
+    //     it(`fetch cell by ID ${i + 1}`, async () => {
+    //         console.log('test execution')
 
-    // const { query } = createTestClient(server);
-
-    // beforeEach(async () => {
-    //     await orm
-    //         .sequelize
-    //         .drop()
-    //         .then(
-    //             async () => {
-    //                 /** any better way of doing this? */
-    //                 const proxyRequire = (path) => {
-    //                     const module = {};
-
-    //                     ((content, module) => eval(content))(fs.readFileSync(path, { encoding: 'utf8' }), module);
-
-    //                     return module.exports;
-    //                 };
-
-    //                 const executeFiles = async (path) => {
-    //                     const acc = [];
-    //                     const files = fs.readdirSync(path);
-    //                     for (const file of files) {
-    //                         const f = `${path}/${file}`;
-
-    //                         try {
-    //                             const migration = proxyRequire(f);
-    //                             debugger;
-    //                             await migration.up(orm.sequelize.queryInterface, orm.Sequelize);
-    //                         } catch (e) {
-    //                             debugger;
-    //                         }
-
-    //                         acc.push(f);
-    //                     }
-
-    //                     return acc;
-    //                 }
-
-    //                 const migrations = await executeFiles(`${__dirname}/../../database/migrations`);
-    //                 const fixtures = await executeFiles(`${__dirname}/../../database/fixtures`);
-
-    //                 console.log('migrations finished');
-    //                 console.log({ migrations, fixtures });
-    //             }
-    //         )
-    // })
-
-    (new Uint8Array(10)).forEach((_, i) => {
-        it(`fetch cell by ID ${i}`, async () => {
-            console.log('test execution')
-
-            const result = await query(
-                {
-                    query: `
-                    {
-                        cell(id: 1) {
-                            seq
-                            id
-                            coordinate
-                        }
-                    }`
-                }
-            );
-
-            expect(result).toMatchSnapshot();
-        });
-    })
-    // it('fetch cell by ID', async () => {
-    //     console.log('test execution')
-
-    //     const result = await query(
-    //         {
-    //             query: `
+    //         const result = await query(
     //             {
-    //                 cell(id: 1) {
-    //                     seq
-    //                     id
-    //                     coordinate
-    //                 }
-    //             }`
-    //         }
-    //     );
+    //                 query: `
+    //                 {
+    //                     cell(id: 1) {
+    //                         seq
+    //                         id
+    //                         coordinate
+    //                     }
+    //                 }`
+    //             }
+    //         );
 
-    //     expect(result).toMatchSnapshot();
+    //         expect(result).toMatchSnapshot();
+    //     });
     // });
+
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        dataSources: () => ({}),
+        context: () => ({ orm }),
+    });
+
+    const { query } = createTestClient(server);
+
+    beforeEach(async () => {
+        await orm
+            .sequelize
+            .drop()
+            .then(
+                async () => {
+                    /** any better way of doing this? */
+                    const proxyRequire = (path) => {
+                        const module = {};
+
+                        ((content, module) => eval(content))(fs.readFileSync(path, { encoding: 'utf8' }), module);
+
+                        return module.exports;
+                    };
+
+                    const executeFiles = async (path) => {
+                        const acc = [];
+                        const files = fs.readdirSync(path);
+                        for (const file of files) {
+                            const f = `${path}/${file}`;
+
+                            try {
+                                const migration = proxyRequire(f);
+                                debugger;
+                                await migration.up(orm.sequelize.queryInterface, orm.Sequelize);
+                            } catch (e) {
+                                debugger;
+                            }
+
+                            acc.push(f);
+                        }
+
+                        return acc;
+                    }
+
+                    const migrations = await executeFiles(`${__dirname}/../../database/migrations`);
+                    const fixtures = await executeFiles(`${__dirname}/../../database/fixtures`);
+
+                    console.log('migrations finished');
+                    console.log({ migrations, fixtures });
+                }
+            )
+    })
+
+    it('fetch cell by ID', async () => {
+        console.log('test execution')
+
+        const result = await query(
+            {
+                query: `
+                {
+                    cell(id: 1) {
+                        seq
+                        id
+                        coordinate
+                    }
+                }`
+            }
+        );
+
+        expect(result).toMatchSnapshot();
+    });
 });
