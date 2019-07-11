05a0452ee5332fe8dc54fa47b4a1c3dc200b12a3
Eugene Matvejev <eugene.matvejev@gmail.com>
2019-07-11 13:21:11 +0100

    add DB_PORT env. variable

--- config/config.js
+++ config/config.js
@@ -3,6 +3,7 @@ const c = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
 };
 const test = {
